9381f0a8b2543236118ce026bfa7b0f20acb2df0
R <r1@M1.local>
2023-09-19 09:17:20 -0400

    Add script `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews2
+
 Clone do site https://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
-
 function Home() {
-    return <h1>Hello World, Rafael.</h1>
-
+  return <h1>Hello World, Rafael.</h1>;
 }
 
 export default Home;
